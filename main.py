0eecf79c532b95e0ea514f28c898ca772f37b095
Stephen Justice <sjusti3@clemson.edu>
2021-11-11 18:45:39 -0500

    Read yaml into object

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 import time
 import yaml
 
-try:
-    import RPi.GPIO as GPIO
-except:
-    from GPIOEmulator.EmulatorGUI import GPIO
+class Instance():
+    name: str
+    file: str
+    pin: int
 
-def load_config():
-    try:
-        with open('config.yaml', 'r') as file:
-            return yaml.safe_load(file)
-    except yaml.YAMLError as err:
-        print(err)
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
 
 def play_track(file):
     print("Playing " + file)
-    # TODO
+    # TODO: implement!
 
 def main():
-    config = load_config()
-    instances = config['instances']
+    with open('config.yaml', 'r') as file:
+        data_map = yaml.safe_load(file)
+        use_emulator = bool(data_map['use_emulator'])
+        instances = [Instance(**i) for i in data_map['instances']]
+
+    if(use_emulator):
+        from GPIOEmulator.EmulatorGUI import GPIO
+    else:
+        import RPi.GPIO as GPIO  # requires RPi OS
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-
     for instance in instances:
-        GPIO.setup(instance['pin'], GPIO.IN)
+        GPIO.setup(instance.pin, GPIO.IN)
 
     while(True):
         for instance in instances:
-            if (GPIO.input(instance['pin'])):
-                play_track(instance['file'])
+            if (GPIO.input(instance.pin)):
+                play_track(instance.file)
                 time.sleep(0.1)
 
 if __name__ == "__main__":
