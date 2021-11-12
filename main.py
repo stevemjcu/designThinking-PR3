f24779fdd5ca75cbea2c3a30571c52f23df2dd7b
Stephen Justice <sjusti3@clemson.edu>
2021-11-12 00:19:29 -0500

    Separated config into its own module

--- main.py
+++ main.py
@@ -1,52 +1,34 @@
+import config
 import pygame
 import time
 import yaml
 
-class Config(yaml.YAMLObject):
-    yaml_tag = u'!Config'
-
-    def __init__(self, **entries):
-        self.emulate: bool
-        self.instances: list[self.Instance]
-        self.__dict__.update(entries)
-
-    class Instance(yaml.YAMLObject):
-        yaml_tag = u'!Instance'
-
-        def __init__(self, **entries):
-            self.name: str
-            self.file: str
-            self.pin: int
-            self.channel: int
-            self.__dict__.update(entries)
+if(config.config.emulate):
+    from GPIOEmulator.EmulatorGUI import GPIO
+else:
+    import RPi.GPIO as GPIO
 
 def play_soundbite(path: str, channel: int):
     sound = pygame.mixer.Sound(path)
     pygame.mixer.Channel(channel).play(sound)
 
 def main():
-    with open('config.yaml', 'r') as file:
-        loader = yaml.Loader
-        config: Config = yaml.load(file, loader)
-
-    if(config.emulate):
-        from GPIOEmulator.EmulatorGUI import GPIO
-    else:
-        import RPi.GPIO as GPIO
-
+    # load
+    print("Loading...")
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    for instance in config.instances:
+    for instance in config.config.instances:
         GPIO.setup(instance.pin, GPIO.IN)
     pygame.init()
-
+    print("...Done!")
+    # run
     try:
         while(True):
-            for instance in config.instances:
-                if (GPIO.input(instance.pin)):
-                    print("Playing " + instance.name + "...")
-                    play_soundbite(instance.file, instance.channel)
-                    time.sleep(0.1)
+            for i in config.config.instances:
+                if (GPIO.input(i.pin)):
+                    print("Playing " + i.name + "!")
+                    play_soundbite(i.file, i.channel)
+                    time.sleep(0.25)
     finally:
         GPIO.cleanup()
         pygame.quit()
