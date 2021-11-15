11af87eebb2353ec8dadee8c0a9c42a81bb8ed99
stevemjcu <43153897+stevemjcu@users.noreply.github.com>
2021-11-15 10:10:21 -0500

    Implement toggle on/off (#2)
    * Implemented toggle
    
    * Implemented toggle
    
    * Restored missing line
    
    * Removed a print statement
    
    Co-authored-by: Stephen Justice <sjusti3@clemson.edu>

--- main.py
+++ main.py
@@ -7,26 +7,36 @@ if(config.emulate):
 else:
     import RPi.GPIO as GPIO
 
-def play_soundbite(path: str, channel: int):
+def play_soundbite(path: str, channel: int, loops: int = 0):
     sound = pygame.mixer.Sound(path)
-    pygame.mixer.Channel(channel).play(sound)
+    pygame.mixer.Channel(channel).play(sound, loops)
+
+def stop_soundbite(channel: int):
+    pygame.mixer.Channel(channel).stop()
 
 def main():
     pygame.init()
+    playing = {}
     # load
     print("Loading...")
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     for i in config.instances:
         GPIO.setup(i.pin, GPIO.IN)
+        playing[i.name] = False
     print("...Done!")
     # run
     try:
         while(True):
             for i in config.instances:
                 if (GPIO.input(i.pin)):
-                    print("Playing: " + i.name)
-                    play_soundbite(i.file, i.channel)
+                    if(not playing[i.name]):
+                        print(i.name)
+                        play_soundbite(i.file, i.channel, -1)
+                        playing[i.name] = True
+                    else:
+                        stop_soundbite(i.channel)
+                        playing[i.name] = False
                     time.sleep(0.25)
     finally:
         GPIO.cleanup()
