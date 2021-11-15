a1494050f338abd231337675c9adee7d19e73f77
stevemjcu <43153897+stevemjcu@users.noreply.github.com>
2021-11-15 08:56:13 -0500

    Add instance to config + cleanup (#1)
    * Added new .wav file
    
    * Cleaned up some code
    
    * Added entry to config file
    
    Co-authored-by: Stephen Justice <sjusti3@clemson.edu>

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ class Config(yaml.YAMLObject):
 
     def __init__(self, **entries):
         self.emulate: bool
-        self.instances: list[Config.Instance]
+        self.instances: list[Instance]
         self.__dict__.update(entries)
 
-    class Instance(yaml.YAMLObject):
-        yaml_tag = u'!Instance'
+class Instance(yaml.YAMLObject):
+    yaml_tag = u'!Instance'
 
-        def __init__(self, **entries):
-            self.name: str
-            self.file: str
-            self.pin: int
-            self.channel: int
-            self.__dict__.update(entries)
+    def __init__(self, **entries):
+        self.name: str
+        self.file: str
+        self.pin: int
+        self.channel: int
+        self.__dict__.update(entries)
 
 with open('config.yaml', 'r') as f:
     config: Config = yaml.load(f, yaml.Loader)
