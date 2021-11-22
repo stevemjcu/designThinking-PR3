809ef7e22650829a104bda35ba0c6d83ac5c7fa7
Stephen Justice <sjusti3@clemson.edu>
2021-11-22 16:53:42 -0500

    Added archive of emulator code

---
+++ archive/config.py
@@ -0,0 +1,22 @@
+import yaml
+
+class Config(yaml.YAMLObject):
+    yaml_tag = u'!Config'
+
+    def __init__(self, **entries):
+        self.emulate: bool
+        self.instances: list[Instance]
+        self.__dict__.update(entries)
+
+class Instance(yaml.YAMLObject):
+    yaml_tag = u'!Instance'
+
+    def __init__(self, **entries):
+        self.name: str
+        self.file: str
+        self.pin: int
+        self.channel: int
+        self.__dict__.update(entries)
+
+with open('config.yaml', 'r') as f:
+    config: Config = yaml.load(f, yaml.Loader)
