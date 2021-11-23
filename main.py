95c2d352fb6ae5d120c54f82dc503f63873e28ed
stevemjcu <43153897+stevemjcu@users.noreply.github.com>
2021-11-22 21:42:10 -0500

    Updated main.py (#5)
    Co-authored-by: Stephen Justice <sjusti3@clemson.edu>

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from config import config
 import pygame
 import time
 
-i2c = busio.I2C(board.SCL, board.SDA)
-mpr121 = adafruit_mpr121.MPR121(i2c)
-
 def play_soundbite(path: str, channel: int, loops: int = 0):
     pygame.mixer.Channel(channel).play(pygame.mixer.Sound(path), loops)
 
@@ -16,6 +13,8 @@ def stop_soundbite(channel: int):
 
 def main():
     pygame.init()
+    i2c = busio.I2C(board.SCL, board.SDA)
+    mpr121 = adafruit_mpr121.MPR121(i2c)
     is_playing = {}
     for i in config.instances:
         is_playing[i.name] = False
